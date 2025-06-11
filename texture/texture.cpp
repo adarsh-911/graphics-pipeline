768e223aa10b3e843d7da02a9ea187fe80e75f2d
adarsh <ee23btech11203@iith.ac.in>
2025-06-12 01:21:13 +0530

    minor changes

--- texture/texture.cpp
+++ texture/texture.cpp
@@ -32,16 +32,11 @@ Color extractColor (int modelInd, int triInd, Vec3 barycentric) {
   std::vector<Vec2uv> texCords = texInst.texcoords;
   int w =  modelTexColors[modelInd].width;
   int h = modelTexColors[modelInd].height;
-  //std::cout << w << " " << h << "\n";
-  //std::cout << idx.v0 << " " << idx.v1 << " " << idx.v2 << "\n";
-  //std::cout << texInst.name << "\n";
   Vec2uv v0 = texCords[idx.v0];
   Vec2uv v1 = texCords[idx.v1];
   Vec2uv v2 = texCords[idx.v2];
   float u = barycentric.x*v0.u + barycentric.y*v1.u + barycentric.z*v2.u;
   float v = barycentric.x*v0.v + barycentric.y*v1.v + barycentric.z*v2.v;
-  //std::cout << u << " " << v << "\n";
-  //std::cout << std::round(u*(w-1)) + std::round(v*(h-1))*w << "\n";
   Color color = modelTexColors[modelInd].pixels[round(u*(w-1)) + round(v*(h-1))*w];
   return color;
 }
